838652efd34b5c60f0ab2c3397bcfabce30fd30d
chenyuyi <chenyuyi@zkyouxi.com>
2019-12-10 17:41:50 +0800

    modified views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    user = {'nickname': 'cyy'}  # fake user
+    return render_template("index.html",
+                           title='Home',
+                           user=user)
