c96ac7af7f1e86ad88190a6b6cfd8fc5c605ae8f
chenyuyi <chenyuyi@zkyouxi.com>
2019-12-16 10:59:30 +0800

    modified User Model

--- app/models.py
+++ app/models.py
@@ -5,6 +5,22 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(120), unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
 
     def __repr__(self):
         return '<User %r>' % self.nickname
