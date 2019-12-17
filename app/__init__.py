4af49f0989702a8acb0af0403d14e94bb51162d7
chenyuyi <chenyuyi@zkyouxi.com>
2019-12-17 10:37:30 +0800

    delete openid

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_openid import OpenID
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -13,6 +12,5 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 lm = LoginManager()
 lm.init_app(app)
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
