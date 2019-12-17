25fb61411d80a554e05a51b57b6b4db9d7bd0145
chenyuyi <chenyuyi@zkyouxi.com>
2019-12-17 16:23:43 +0800

    add register view

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, login_required
 from app import app, lm
-from app.forms import LoginForm
-from app.models import User
+from app.forms import LoginForm, RegisterForm
+from app.models import User, db
 
 
 @login_required
@@ -18,9 +18,23 @@ def index():
                            posts=posts)
 
 
-@app.route('/register', methods=['GET',POST])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if request.method == 'POST':
+        if not form.validate_on_submit():
+            flash(form.errors)
+            return redirect(render_template('register.html', title='Register', form=form))
+        if form.password1.data != form.password2.data:
+            flash('passwords did not match')
+            return redirect(render_template('register.html', title='Register', form=form))
+
+        user = User(username = form.username, password = form.password1.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('successfully registered')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -45,5 +59,3 @@ def login():
 @lm.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
