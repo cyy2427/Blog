0f589f7d66a5836666f250b988b16c51ab4b2899
chenyuyi <chenyuyi@zkyouxi.com>
2019-12-18 13:31:24 +0800

    use PasswordField

--- app/forms.py
+++ app/forms.py
@@ -1,20 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField
+from wtforms import StringField, BooleanField, SubmitField, TextAreaField, PasswordField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired('username is null')])
-    password = StringField('password', validators=[DataRequired('password is null')])
+    password = PasswordField('password', validators=[DataRequired('password is null')])
     remember_me = BooleanField('remember_me', default=False)
 
 
 class RegisterForm(FlaskForm):
     username = StringField('username', validators=[DataRequired('username is null')])
-    password1 = StringField('password', validators=[DataRequired('password is null')])
-    password2 = StringField('password', validators=[DataRequired('password is null')])
+    password1 = PasswordField('password', validators=[DataRequired('password is null')])
+    password2 = PasswordField('password', validators=[DataRequired('password is null')])
 
 
 class PostForm(FlaskForm):
-    post = StringField('Post:', validators=[DataRequired('post is null')])
+    post = TextAreaField('Post:', validators=[DataRequired('post is null')])
     submit = SubmitField('Submit')
