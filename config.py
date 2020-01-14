b79a48597f3a5fef334e7694bbd553884542da52
chenyuyi <chenyuyi@zkyouxi.com>
2020-01-14 19:07:03 +0800

    code refractored

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-UPLOADED_ICON_DEST = os.path.join(basedir, 'app/static/uploads')
-CKEDITOR_SERVE_LOCAL = True
-
-CSRF_ENABLED = True
-SECRET_KEY = 'hello-flask'
